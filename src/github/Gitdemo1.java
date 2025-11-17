03d990aaa28988399d52f1de0244b18ad7ce29d0
goswamypp <goswamypp96@gmail.com>
2025-11-17 18:20:22 +0530

    add all

--- src/github/Gitdemo1.java
+++ src/github/Gitdemo1.java
@@ -1,5 +1,8 @@
 package github;
 
 public class Gitdemo1 {
+	public void funA(){
+		System.out.println("");
+	}
 
 }
--- src/github/gitdemo3.java
+++ src/github/gitdemo3.java
@@ -1,5 +1,10 @@
 package github;
 
 public class gitdemo3 {
+	public void funB(){
+		System.out.println("git demo3");
+		System.out.println("done");
+		
+	}
 
 }
